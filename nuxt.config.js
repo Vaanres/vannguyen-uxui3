f2e86f8f45d74eb94e620e722d1c30bea0d5660a
Vaanres <Vaanres@Vaanres-Mac.local>
2019-06-21 17:16:50 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
         rel: 'stylesheet',
         lazyload: true,
         href:
-          'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=optional&subset=vietnamese'
+          'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap&subset=vietnamese'
       }
     ]
   },
