cc702a5e887be8ce78d2e2ca30fe4e5645d0e5c0
Vaanres <Vaanres@Vaanres-Mac.local>
2019-09-13 12:55:06 +0700

    update

--- components/pages/home/HomeHero.vue
+++ components/pages/home/HomeHero.vue
@@ -6,9 +6,7 @@
         <div class="col-12 col-md-8">
           <p class="tagline tagline--primary">{{ tagline }}</p>
           <h1 class="title mb-4">{{ title }}</h1>
-          <p class="subTitle sub-title">
-            {{ subTitle }}
-          </p>
+          <p class="subTitle sub-title">{{ subTitle }}</p>
           <btn-action class="btn-hero mt-5" link="/about"
             >Know more about me</btn-action
           >
